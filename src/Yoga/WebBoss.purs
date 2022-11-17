e5bdd5f37ea6f1376733382b6ca7b800ea29dd40
Mark Eibes <mark.eibes@gmail.com>
2022-11-17 10:52:23 +0100

    Get the types right

--- src/Yoga/WebBoss.purs
+++ src/Yoga/WebBoss.purs
@@ -6,8 +6,8 @@ import Effect (Effect)
 import Yoga.WebProletarian.Types (Worker)
 
 foreign import onMessageFromWorker
-  ∷ ∀ toBoss toWorker. (toBoss → Effect Unit) → Worker toBoss toWorker → Effect Unit
+  ∷ ∀ toWorker toBoss. (toBoss → Effect Unit) → Worker toWorker toBoss → Effect Unit
 
-foreign import postMessageToWorker ∷ ∀ toBoss toWorker. toWorker → Worker toBoss toWorker → Effect Unit
+foreign import postMessageToWorker ∷ ∀ toBoss toWorker. toWorker → Worker toWorker toBoss → Effect Unit
 
-foreign import terminate ∷ ∀ toBoss toWorker. Worker toBoss toWorker → Effect Unit
+foreign import terminate ∷ ∀ toWorker toBoss. Worker toWorker toBoss → Effect Unit
